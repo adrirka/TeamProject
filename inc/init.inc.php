706154e89c4f9266d9a5cf8d95c26ef175782409
adrirka <rousselet.adrien@gmail.com>
2017-05-22 12:50:14 +0200

    connexion file et updates inc

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -1,7 +1,7 @@
 <?php 
 
 // Connexion à la BDD
-$pdo = new PDO('mysql:host=localhost;dbname=annonceo', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$pdo = new PDO('mysql:host=localhost;dbname=salles', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 // Initialiser le début de session
 session_start();
