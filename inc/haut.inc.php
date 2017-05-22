532ff1e667b6521a5cd095bc4e4ce0f3d6bc13e8
ARLineAR <alinerambier@gmail.com>
2017-05-22 15:15:06 +0200

    inscription2

--- inc/haut.inc.php
+++ inc/haut.inc.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Location de salles/title>
+    <title>Location de salles</title>
 
     <link rel="stylesheet" href="<?php echo RACINE_SITE . 'inc/css/style.css'; ?>">
     
